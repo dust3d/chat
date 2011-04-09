06a71fd30eaaa89ea349017c093d011ef3eaeeb7
Dustin Anderson <dkanderson@gmail.com>
2011-04-09 16:44:41 -0500

    adding location spec and model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates_associated :user
   validates :chat_input, :presence => true
+  
+  before_save :geocode
+  
+  def geocode
+    unless (self.lat.blank? || self.lon.blank?)
+      loc=Geokit::Geocoders::GoogleGeocoder.reverse_geocode([self.lat, self.lon])
+      self[:location] = "#{loc.city}, #{loc.state}" if loc.success
+    end
+  end
+  
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe Post do
-  
+
   it "requires chat input" do
     User.delete_all
     Post.delete_all
     Post.new.should_not be_valid
     Post.new(:chat_input => "Hi!", :user => Factory(:user)).should be_valid
+  end
+
+  it "saves location" do
+    p = Post.new(:chat_input => "Hi!", :user => Factory(:user), :lat => 41.8781136, :lon => -87.6297982 )
+    p.save
+    p.location.should == "Chicago, IL"
   end
 end
