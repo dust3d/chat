663539ee70742a1ccd7d2143e16b1ad2c2a1279d
Keith Norman <keithn@groupon.com>
2011-04-09 23:40:57 -0500

    fix merge conflict

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,5 @@
 if (navigator.userAgent.match(/Mobile|WebOS/i)) var mobile = true;
+var env;
 
 $(function() {
   $('#chat_input').keypress(function(){
@@ -25,9 +26,39 @@ $(function() {
     }
   });
   
-  $('#chat_input').focus();
-});
+  env = $('#rails_env').data('value');
 
+  var pusher = new Pusher('534d197146cf867179ee');
+  var channel = pusher.subscribe('groupon_go_' + env);
+
+  pusher.bind('new_post',
+    function(data) {
+      var tmpl = MustacheTemplates['chats/_post'];
+      var post = {
+        profile_image_url: data.profile_image_url,
+        twitter_login: data.twitter_login,
+        name: data.name,
+        login: data.twiter_login,
+        chat_input: data.body,
+        time_ago: 'less than a minute'
+      };
+      $('#chat_data').prepend(Mustache.to_html(tmpl, post));
+    }
+  );
+  
+  $('#chat_input').focus();
+
+  $("#chat_input").keyup(function(e){
+    if($(this).val().match(/@[\S]+/i)){
+      var q = $(this).val().match(/(?:@)([\S]+)/i)[1];
+      $.get('/users.json?q=' + q, function(data){
+        var current_val = $('#chat_input').val();
+        $('#chat_input').val(current_val.replace(q, data[0].name));
+      });
+    }
+  });
+  
+});
 
 (function($) {
   $.fn.request = function(success) {
@@ -40,27 +71,3 @@ $(function() {
     $.ajax(jqopts);
   };
 })(jQuery);
-
-var pusher = new Pusher('534d197146cf867179ee');
-
-var channel = pusher.subscribe('groupon_go');
-
-pusher.bind('new_post',
-  function(data) {
-    var tmpl = MustacheTemplates['chats/_post'];
-    var post = {
-      profile_image_url: data.profile_image_url,
-			twitter_login: data.twitter_login,
-      name: data.name,
-      chat_input: data.body,
-      time_ago: 'less than a minute'
-    };
-    if (mobile) {
-      $('#chat_data').append(Mustache.to_html(tmpl, post));
-    } else {
-      $('#chat_data').prepend(Mustache.to_html(tmpl, post));
-    }
-    
-  }
-);
-
